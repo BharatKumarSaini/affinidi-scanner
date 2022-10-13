b3d9fbe86aca5c3d8c9219d5f5f4649ff1ceba36
BharatKumarSaini <bharatskataria@gmail.com>
2022-10-13 17:59:26 +0530

    name set to counter

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from 'react'
-import RNAffinidiModule, { Scanner } from 'affinidi-scanner'
+import RNAffinidiModule, { Counter } from 'affinidi-scanner'
 
 const App = () => {
   useEffect(() => {
     console.log(RNAffinidiModule)
   })
 
-  return <Scanner />
+  return <Counter />
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react'
 import { NativeModules, StyleSheet, View } from 'react-native'
 
+//import Scanner from './components/scanner'
 import App from './App'
 //import Home from './components/home'
 //import HomeStack from './routes/homeStack'
-export const Scanner = () => {
+export const Counter = () => {
   return (
     <View style={styles.AppContainer}>
       <App />
