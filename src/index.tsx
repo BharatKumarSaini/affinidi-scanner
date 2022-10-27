9675c1da3cf76bd358ce03adf54892ea5e71fc6c
BharatKumarSaini <bharatskataria@gmail.com>
2022-10-27 13:34:24 +0530

    colour changed

--- src/index.tsx
+++ src/index.tsx
@@ -18,7 +18,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#000',
+    backgroundColor: '#ffff',
 
     flexDirection: 'column-reverse',
 
